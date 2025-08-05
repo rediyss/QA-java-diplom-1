f53913802a340e56a9fee489f8d150be1e6d100a
rediyss <reddreynoklik@yandex.ru>
2025-08-05 18:10:21 +1100

    Описание страницы логирования

---
+++ src/test/java/praktikum/page/LoginPage.java
@@ -0,0 +1,22 @@
+
+package praktikum.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private final WebDriver driver;
+    private final By emailField = By.xpath("//input[@name='name']");
+    private final By passwordField = By.xpath("//input[@name='Пароль']");
+    private final By loginButton = By.xpath("//button[text()='Войти']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String email, String password) {
+        driver.findElement(emailField).sendKeys(email);
+        driver.findElement(passwordField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+}
