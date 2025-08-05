1e3af46d4af0a2b91e2cd0bb26a50dd58d0667d1
rediyss <reddreynoklik@yandex.ru>
2025-08-05 18:12:09 +1100

    страница забыл пароль

---
+++ src/test/java/praktikum/page/ForgotPasswordPage.java
@@ -0,0 +1,18 @@
+
+package praktikum.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ForgotPasswordPage {
+    private final WebDriver driver;
+    private final By loginLink = By.xpath("//a[text()='Войти']");
+
+    public ForgotPasswordPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginLink() {
+        driver.findElement(loginLink).click();
+    }
+}
