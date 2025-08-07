a26f7ed51ae69ae62436b7d933f852fee0b59787
rediyss <reddreynoklik@yandex.ru>
2025-08-07 17:54:02 +1100

    Для всех зависимостей используются моки  Разделены тесты

--- src/test/java/praktikum/BurgerParameterizedAddIngredientTest.java
+++ src/test/java/praktikum/BurgerParameterizedAddIngredientTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(Parameterized.class)
 public class BurgerParameterizedAddIngredientTest {
@@ -27,10 +28,10 @@ public class BurgerParameterizedAddIngredientTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> getIngredients() {
-        return Arrays.asList(new Object[][] {
-                { IngredientType.SAUCE, "ketchup", 0.5f },
-                { IngredientType.FILLING, "beef", 1.2f },
-                { IngredientType.SAUCE, "mustard", 0.4f }
+        return Arrays.asList(new Object[][]{
+                {IngredientType.SAUCE, "ketchup", 0.5f},
+                {IngredientType.FILLING, "beef", 1.2f},
+                {IngredientType.SAUCE, "mustard", 0.4f}
         });
     }
 
@@ -40,10 +41,26 @@ public class BurgerParameterizedAddIngredientTest {
     }
 
     @Test
-    public void testAddIngredientParameterized() {
-        Ingredient ingredient = new Ingredient(type, name, price);
-        burger.addIngredient(ingredient);
+    public void testIngredientListSizeAfterAddition() {
+        Ingredient mockIngredient = mock(Ingredient.class);
+        when(mockIngredient.getType()).thenReturn(type);
+        when(mockIngredient.getName()).thenReturn(name);
+        when(mockIngredient.getPrice()).thenReturn(price);
+
+        burger.addIngredient(mockIngredient);
+
         assertEquals(1, burger.ingredients.size());
-        assertEquals(ingredient, burger.ingredients.get(0));
+    }
+
+    @Test
+    public void testCorrectIngredientAddedToBurger() {
+        Ingredient mockIngredient = mock(Ingredient.class);
+        when(mockIngredient.getType()).thenReturn(type);
+        when(mockIngredient.getName()).thenReturn(name);
+        when(mockIngredient.getPrice()).thenReturn(price);
+
+        burger.addIngredient(mockIngredient);
+
+        assertEquals(mockIngredient, burger.ingredients.get(0));
     }
 }
